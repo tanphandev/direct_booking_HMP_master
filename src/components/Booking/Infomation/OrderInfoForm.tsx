4094d1235eed0feb801231ffdedd99cb2a1df03e
= <phantanbn31@gmail.com>
2023-11-12 12:18:46 +0700

    style: Auto Complete Countries

--- src/components/Booking/Infomation/OrderInfoForm.tsx
+++ src/components/Booking/Infomation/OrderInfoForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import InputField from '@/components/common/InputField/InputField';
+import CountriesAutocomplete from '@/components/common/AutoComplete/CountriesAutocomplete';
 
 function OrderInfoForm() {
   const [orderFullName, setOrderFullName] = useState<string>('');
@@ -43,17 +44,7 @@ function OrderInfoForm() {
             setOrderFullName(e.target.value);
           }}
         />
-        <InputField
-          labelClassName="text-base text-grey-21 mb-2"
-          inputClassName="text-base text-grey-21 px-[12px] py-[6px] border-[1px] border-grey-d9 focus:border-blue-80 focus:shadow-[0_0_0_0.2rem_rgba(0,123,255,.25)] rounded-md"
-          isRequire={true}
-          id="order-Country"
-          label="Country/region of residence"
-          value={orderFullName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setOrderFullName(e.target.value);
-          }}
-        />
+        <CountriesAutocomplete isRequire={true} />
       </div>
     </div>
   );
