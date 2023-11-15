9faf221cf4f915c00f8b17efda1c1115480ee8cc
= <phantanbn31@gmail.com>
2023-11-15 11:19:19 +0700

    fix: fix hotel slug

--- src/routes/Path.ts
+++ src/routes/Path.ts
@@ -2,6 +2,7 @@ const Path = {
   NOT_FOUND: '/not-found',
   HOME: (hotel_slug: string) => `/${hotel_slug}`,
   BOOKING: (hotel_slug: string) => `/${hotel_slug}/booking`,
+  SEARCH: (hotel_slug: string) => `/${hotel_slug}/search`,
   ABOUT: (hotel_slug: string) => `/${hotel_slug}/about`,
   FAQS: (hotel_slug: string) => `/${hotel_slug}/faqs`,
   ENGLIST_DEMO_TEST: (hotel_slug: string) => `/${hotel_slug}/english-test`,
