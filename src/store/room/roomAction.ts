b6b073427e15eef75f7fc5bcb8fb19072966df4d
yenle <yenle16032002@gmail.com>
2023-11-17 15:04:53 +0700

    clean

--- src/store/room/roomAction.ts
+++ src/store/room/roomAction.ts
@@ -1,6 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
-const getBusiness: any = createAction('business/getBusiness');
-const checkCouponCode: any = createAction('business/checkCouponCode');
+
 const getPublicRoomAvailable: any = createAction('room/getPublicRoomAvailable');
-export { getBusiness, checkCouponCode, getPublicRoomAvailable };
+export { getPublicRoomAvailable };
--- src/store/room/roomSaga.ts
+++ src/store/room/roomSaga.ts
@@ -5,72 +5,19 @@ import API from '@/api/api';
 import ApiCaller from '@/api/ApiCaller';
 import * as businessAction from './roomAction';
 import {
-  checkCouponFailed,
-  getBasicBusinessInfoFailed,
-  getBasicBusinessInfoSuccess,
-  getBusinessPFSuccess,
-  getFooterNavigationSuccess,
-  getPaymentsMethodSuccess,
-  getSettingSuccess,
-
-  
   getPublicRoomAvailableSuccess,
   getPublicRoomAvailableFailed
 } from './roomSlice';
 import { disableLoading, enableLoading } from '../common/commonSlice';
-import { BUSINESS, CHECK_COUPON_CODE, PUBLIC_ROOM_AVAILABLE } from '../common/constants';
-
-import Path from '@/routes/Path';
-import { toast } from 'react-toastify';
-import { basic_business_info_field } from '../business/constant';
-
-function* getBusiness({ payload }: any): Generator {
-  const { business_slug, router } = payload;
-  yield put(enableLoading(BUSINESS));
-  try {
-    const data: any = yield call(ApiCaller.get, API.get_business_by_slug, { slug: business_slug });
-    const basic_business_info = pick(data, basic_business_info_field);
-    yield put(getBasicBusinessInfoSuccess(basic_business_info));
-    yield put(getBusinessPFSuccess(data.business_pf));
-    yield put(getSettingSuccess(data.direct_booking_setting));
-    yield put(getFooterNavigationSuccess(data.direct_booking_setting.navigation));
-    yield put(getPaymentsMethodSuccess(data.payment_method));
-  } catch (error: any) {
-    yield put(getBasicBusinessInfoFailed(error));
-    /* check bussiness not found */
-    if (error?.response?.status === 406) {
-      router.push(Path.NOT_FOUND);
-    }
-  } finally {
-    yield put(disableLoading(BUSINESS));
-  }
-}
-
-function* checkCouponCode({ payload }: any): Generator {
-  const { bodyData } = payload;
-  yield put(enableLoading(CHECK_COUPON_CODE));
-  try {
-    const data: any = yield call(ApiCaller.post, API.direct_booking_check_coupon_code, bodyData);
-    if (!data[0]) {
-      toast.error('Mã của bạn không hợp lệ');
-    } else {
-      toast.success('Mã đã được áp dụng');
-    }
-  } catch (error: any) {
-    yield put(checkCouponFailed(error));
-  } finally {
-    yield put(disableLoading(CHECK_COUPON_CODE));
-  }
-}
+import { PUBLIC_ROOM_AVAILABLE } from '../common/constants';
 
 function* getPublicRoomAvailable({ payload }: any): Generator {
 
   const { bid, parameters, datecreated } = payload;
   const urlSearchParams = new URLSearchParams(parameters);
-  const checkin = (urlSearchParams.get('check_in') );
-  const checkout = (urlSearchParams.get('check_out') );
+  const checkin = (urlSearchParams.get('check_in'));
+  const checkout = (urlSearchParams.get('check_out'));
   yield put(enableLoading(PUBLIC_ROOM_AVAILABLE));
-
   try {
     const data: any = yield call(ApiCaller.get, API.public_room_available, {
       bid,
@@ -80,6 +27,8 @@ function* getPublicRoomAvailable({ payload }: any): Generator {
       },
       datecreated,
     });
+  
+
     yield put(getPublicRoomAvailableSuccess(data));
 
   } catch (error: any) {
@@ -89,11 +38,8 @@ function* getPublicRoomAvailable({ payload }: any): Generator {
   }
 }
 
-export default function* businessSaga() {
+export default function* RoomSaga() {
   yield all([
-    takeLatest(businessAction.getBusiness, getBusiness),
-    takeLatest(businessAction.checkCouponCode, checkCouponCode),
-    
     takeLatest(businessAction.getPublicRoomAvailable, getPublicRoomAvailable),
   ]);
 }
