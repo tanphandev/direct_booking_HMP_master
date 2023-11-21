dbc53cc61eda1c57344aebce904bd21d0014a92a
yenle <yenle16032002@gmail.com>
2023-11-21 09:43:09 +0700

    call api: setup store commonPage

---
+++ src/store/commonPages/commonPagesSaga.ts
@@ -0,0 +1,32 @@
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+
+import API from '@/api/api';
+import ApiCaller from '@/api/ApiCaller';
+import { disableLoading, enableLoading } from '../common/commonSlice';
+import { COMMON_PAGES } from '../common/constants';
+import { getCommonPageFailed, getCommonPageSuccess } from './commonPagesSlice';
+import * as commonPagesAction from './commonPagesAction';
+
+function* getCommonPages( {payload} : any): Generator {
+  const  {hotel_slug, pages_slug, datecreated,router}  = payload;
+  yield put(enableLoading(COMMON_PAGES));
+  try {
+    const data: any = yield call(ApiCaller.get, API.get_common_pages_by_slug, {
+      hotel_slug,
+      pages_slug,
+      datecreated,
+    });
+  
+
+    yield put(getCommonPageSuccess(data));
+  } catch (error: any) {
+    yield put(getCommonPageFailed(error));
+  } finally {
+    yield put(disableLoading(COMMON_PAGES));
+  }
+}
+export default function* CommonPagesSaga() {
+  yield all([
+    takeLatest(commonPagesAction.getCommonPages, getCommonPages),
+  ]);
+}
